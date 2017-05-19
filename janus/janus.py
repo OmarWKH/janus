bdc1ea0b0f26755475dfde7a161ff7f17f28528a
Omar Khashoggi <omar.w.kh@gmail.com>
2017-05-19 21:42:25 +0300

    add albaraa play test

--- janus/janus.py
+++ janus/janus.py
@@ -1,3 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+
+@app.route('/playtest')
+def playtest():
+	return render_template('playtest.html')
---
+++ janus/static/playtest.js
@@ -0,0 +1,104 @@
+story_json = { "Story": {
+		"id" : "STORY_ID",
+        "title": "Vengence",
+		"Author" : "Albaraa \"Vonrex\" Bajnaid",
+		"Events" : [
+				{ "Event_id" : "0",
+				  "Event_title" : "Pain",
+				  "Event_Content" : "Here eyes … Full of tears, She’s Gasping for air now!.",
+				  "Default_branch" : [
+								{"choice" : "You ran to her out of chivalry to comfort her.", "Next_event": "1"},
+								{"choice" : "You said “there there’, with the tip of your tongue, and continued your way.", "Next_event": "0"},
+                                {"choice": "You just … left.", "Next_event": "2"}
+						     ]
+				 
+							
+				},
+				{ "Event_id" : "1",
+				  "Event_title" : "EVENT_TITLE_1",
+                  "Event_Content" : "TEXT_2",
+				  "Default_branch" :  [ 
+								{ "choice" : "Choice_Text", "Next_event": "0" },
+								{ "choice" : "Choice_Text_2", "Next_event": "1" }
+						      ]
+				},
+                { "Event_id" : "2",
+				  "Event_title" : "Anger",
+                  "Event_Content" : "Behind your back, weird screeches started popping out, the crying stopped, the screeches transformed into crackling of teeth, painful.  The sound is getting closer and closer to you.",
+				  "Default_branch" :  [ 
+								{ "choice" : "You ran like you’ve never ran before.", "Next_event": "0" },
+								{ "choice" : "You turned cowardly, yet curiously!.", "Next_event": "1" },
+                                { "choice": "You grabbed a stone from the ground beneath you, and turned to face the demon!.", "Next_event": "0"},
+                                { "choice" : "You kept walking as if nothing’s happened.", "Next_event": "3" }
+						      ]
+                },
+                {
+                    "Event_id" : "3",
+				  "Event_title" : "Death",
+                  "Event_Content" : "A Scream pierced your ears, your bones started to shatter, your vision...Darkened.",
+				  "Default_branch" :  [ 
+								{ "choice" : "The End", "Next_event": "4", "end" : true },
+						      ]
+                },
+			     ]
+		},
+
+
+} ;
+
+story = "";
+window.addEventListener("load", start);
+
+ function start(){
+     console.log("Starting...");
+     //var story = JSON.parse(story_json);
+     story = story_json;
+     console.log("JSON file parsed...");
+     load_event(0);
+     
+
+ }
+
+ function load_event(id){
+    var author = story.Story.Author;
+    console.log(author);
+    var event_id = story.Story.Events[id].Event_id;
+    var event_title = story.Story.Events[id].Event_title;
+    document.getElementById("E_data").innerHTML = 
+    "<h4>" + event_id + ":" + event_title + "</h4>";
+
+    var event_text = story.Story.Events[id].Event_Content;
+    console.log(event_text);
+    document.getElementById("E_text").innerHTML = 
+    "<p>" + event_text + "</p>";
+
+    document.getElementById("E_choices").innerHTML = "";
+    console.log(story.Story.Events[id].Default_branch);
+    var branches = story.Story.Events[id].Default_branch;
+    for(i = 0; i < branches.length; i++){
+        console.log(branches[i]);
+        choice(branches[i]);
+    }
+ }
+
+ function choice(branch){
+     console.log(branch.choice);
+    var choice_text = branch.choice;
+    var choice_target = branch.Next_event;
+    console.log(choice_target);
+    if( branch.end === true){
+        document.getElementById("E_choices").innerHTML += 
+    "<button type=\"button\" title=\"" + choice_text + 
+     "\" onClick=\"end_event("+
+     choice_target+")\">"+choice_text+"</button><br>"
+    }else {
+        document.getElementById("E_choices").innerHTML += 
+        "<button type=\"button\" title=\"" + choice_text + 
+        "\" onClick=\"load_event("+
+        choice_target+")\">"+choice_text+"</button><br>";
+    }
+ }
+
+ function end_event(id){
+     console.log("Story ended at Event: "+id);
+ }
