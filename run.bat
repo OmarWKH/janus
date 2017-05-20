b8fea53452639245f70f73dde822901a3486e6b7
Omar Khashoggi <omar.w.kh@gmail.com>
2017-05-20 11:30:57 +0300

    windows script to run development server

---
+++ run.bat
@@ -0,0 +1,14 @@
+@echo off
+REM runs flask's development server, reference: http://flask.pocoo.org/docs/0.12/server/
+
+where flask
+if %ERRORLEVEL% neq 0 (
+	echo  Either flask was not installed or you forgot to turn on your python virtual environment.
+	echo  To install flask: 'pip install -r requirements.txt'
+	echo   Make sure you are using the desired pytohn venv before installing.
+	echo  To create a virtual environment, if desired: "https://docs.python.org/3/tutorial/venv.html"
+) else (
+	set FLASK_APP=janus/janus.py
+	set FLASK_DEBUG=1
+	flask run
+)
