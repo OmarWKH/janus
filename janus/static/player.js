1f81a0215add752b8a472dabf8d2b6127d1648f4
Albaraa Bajnaid <bajnaid.albaraa@gmail.com>
2017-05-21 21:59:30 +0300

    Initial Save Functionality
    Added Save function and called it when an event loads, printed save in
    console after ending sequence.

--- janus/static/player.js
+++ janus/static/player.js
@@ -1,78 +1,86 @@
 function player() {
-	// window.addEventListener("load", start);
-	
-	this.start = function(story_json){
-		 console.log("Starting...");
-		 this.story = JSON.parse(story_json);
-		 console.log("JSON file parsed...");
-		 load_event(0);
-	}
+    // window.addEventListener("load", start);
 
-	this.load_event = function(id){
-		var author = this.story.Story.Author;
-		console.log(author);
-		var event_id = this.story.Story.Events[id].Event_id;
-		var event_title = this.story.Story.Events[id].Event_title;
-		document.getElementById("E_data").innerHTML = 
-		"<h4>" + event_id + ":" + event_title + "</h4>";
+    function saveProg(event_id){
+        console.log('Saving...');
+        save.last_event = event_id;
+        save.path.push(event_id);
+    }
 
-		var event_text = this.story.Story.Events[id].Event_Content;
-		console.log(event_text);
-		document.getElementById("E_text").innerHTML = 
-		"<p>" + event_text + "</p>";
+    this.start = function(story_json){
+        this.story = JSON.parse(story_json);
+        console.log("JSON file parsed...");
+        load_event(0);
+    }
 
-		document.getElementById("E_choices").innerHTML = "";
-		console.log(this.story.Story.Events[id].Default_branch);
-		var branches = this.story.Story.Events[id].Default_branch;
-		for(i = 0; i < branches.length; i++){
-				console.log(branches[i]);
-				choice(branches[i]);
-		}
-	}
+    this.load_event = function(id){
+        var author = this.story.Story.Author;
+        console.log(author);
+        var event_id = this.story.Story.Events[id].Event_id;
+        var event_title = this.story.Story.Events[id].Event_title;
+        saveProg(event_id);
+        document.getElementById("E_data").innerHTML =
+            "<h4>" + event_id + ":" + event_title + "</h4>";
 
-	this.choice = function(branch){
-		console.log(branch.choice);
-		var choice_text = branch.choice;
-		var choice_target = branch.Next_event;
-		console.log(choice_target);
-		if( branch.end === true){
-				document.getElementById("E_choices").innerHTML += 
-		"<button type=\"button\" title=\"" + choice_text + 
-		 "\" onClick=\"end_event("+
-		 choice_target+")\">"+choice_text+"</button><br>"
-		}else {
-				document.getElementById("E_choices").innerHTML += 
-				"<button type=\"button\" title=\"" + choice_text + 
-				"\" onClick=\"load_event("+
-				choice_target+")\">"+choice_text+"</button><br>";
-		}
-	}
+        var event_text = this.story.Story.Events[id].Event_Content;
+        console.log(event_text);
+        document.getElementById("E_text").innerHTML =
+            "<p>" + event_text + "</p>";
 
-	this.end_event = function(id){
-		 console.log("Story ended at Event: "+id);
-	}
+        document.getElementById("E_choices").innerHTML = "";
+        console.log(this.story.Story.Events[id].Default_branch);
+        var branches = this.story.Story.Events[id].Default_branch;
+        for(i = 0; i < branches.length; i++){
+            console.log(branches[i]);
+            choice(branches[i]);
+        }
+    }
 
-	return this.start;
+    this.choice = function(branch){
+        console.log(branch.choice);
+        var choice_text = branch.choice;
+        var choice_target = branch.Next_event;
+        console.log(choice_target);
+        if( branch.end === true){
+            document.getElementById("E_choices").innerHTML +=
+                "<button type=\"button\" title=\"" + choice_text +
+                "\" onClick=\"end_event("+
+                choice_target+")\">"+choice_text+"</button><br>"
+        }else {
+            document.getElementById("E_choices").innerHTML +=
+                "<button type=\"button\" title=\"" + choice_text +
+                "\" onClick=\"load_event("+
+                choice_target+")\">"+choice_text+"</button><br>";
+        }
+    }
+
+    this.end_event = function(id){
+        console.log("Story ended at Event: "+id);
+        console.log("Save:");
+        console.log(save);
+    }
+
+    return this.start;
 }
 
 function request_story(id) {
-	let url = window.location.protocol + "//" + window.location.host + "/story_json/" + id;
-	httpGetAsync(url, player());
+    let url = window.location.protocol + "//" + window.location.host + "/story_json/" + id;
+    httpGetAsync(url, player());
 }
 
 // https://stackoverflow.com/a/4033310
 // alt: fetch https://stackoverflow.com/a/38297729
 function httpGetAsync(url, callback) {
-	let xmlHttp = new XMLHttpRequest();
-	xmlHttp.onreadystatechange = function() {
-		const done = 4;
-		const ok = 200;
-		if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-			callback(xmlHttp.responseText);
-		}
-	}
-	let async = true;
-	console.log(url);
-	xmlHttp.open("GET", url, async);
-	xmlHttp.send();
+    let xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() {
+        const done = 4;
+        const ok = 200;
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+            callback(xmlHttp.responseText);
+        }
+    }
+    let async = true;
+    console.log(url);
+    xmlHttp.open("GET", url, async);
+    xmlHttp.send();
 }
